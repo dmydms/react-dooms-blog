8e184697deffec2c907a964cf5dacddc58fa8814
dhdh3311 <dhdh3311@naver.com>
2021-09-22 16:40:36 +0900

    feat: `Next.js`, `sass` 설치

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# react-dooms-blog
+This is a starter template for [Learn Next.js](https://nextjs.org/learn).
---
+++ pages/index.jsx
@@ -0,0 +1,30 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      
+
+      <style jsx global>{`
+        html,
+        body {
+          background-color: black;
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
