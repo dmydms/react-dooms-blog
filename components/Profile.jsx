3c57d613787064ccd3f7643aab4a98395dca2c2e
dhdh3311 <dhdh3311@naver.com>
2021-09-26 00:16:01 +0900

    feat: WHO AM I 섹션 추가 - 일부 디자인 수정

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,17 +1,39 @@
 import styles from './Profile.module.scss'
+import Link from 'next/Link'
 
 export default function Profile() {
     return (
         <div className={styles.root}>
-            <img className={styles.profile_img} src={'/images/profile_img.jpg'} alt="Picture of the author" />
-            <div className={styles.profile_desc_wrapper}>
-                <h2>doomdoom</h2>
-                <p>그림 그리기 🎨, 클래식 듣기 🎻 를 좋아하는 <br/>관심사가 다채로운 풀스택 개발자 doomdoom 입니다. </p>
-                <ul>
-                    <li><a href={'https://twitter.com/doomydoomydooms'} target={'_blank'}>TWITTER&nbsp;&nbsp;&nbsp;</a></li>
-                    <li><a href={'https://doomydoomydooms.medium.com/'} target={'_blank'}>BLOG&nbsp;&nbsp;&nbsp;</a></li>
+            <section className={styles.profile_section}>
+                <img className={styles.profile_img} src={'/images/profile_img.jpg'} alt="Picture of the author" />
+                <div className={styles.profile_desc_wrapper}>
+                    <h2>doomdoom</h2>
+                    <p> 안녕하세요! 풀스택 개발자 doomdoom 입니다. <br/> JavaScript, Python을 주로 사용합니다. </p>
+                    <div className={styles.favorites_wrapper}>
+                        <h2 className={styles.sub_title}>WHAT I LOVE</h2>
+                        <ul className={styles.favorites}>
+                            <li className={styles.favorites_item}>Reading 📚</li>
+                            <li className={styles.favorites_item}>Coding 👩‍💻</li>
+                            <li className={styles.favorites_item}>Drawing 🎨</li>
+                            <li className={styles.favorites_item}>Classic 🎹</li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+            <section className={styles.profile_section}>
+                <h2 className={styles.sub_title}>WHO AM I</h2>
+                <ul className={styles.btns_wrapper}>
+                    <a target={'_blank'} href={'https://iced-farmer-712.notion.site/Full-Stack-Developer-1292017ba65e4e558213042533285032'}>
+                        <li className={styles.round_btn}>Summary</li>
+                    </a>
+                    <a target={'_blank'} href={'https://iced-farmer-712.notion.site/Full-Stack-Developer-1292017ba65e4e558213042533285032'}>
+                        <li className={styles.round_btn}>I started development as Front-end developer</li>
+                    </a>
+                    <a target={'_blank'} href={'https://iced-farmer-712.notion.site/Full-Stack-Developer-1292017ba65e4e558213042533285032'}>
+                        <li className={styles.round_btn}>However, Back-end development is also my main task.</li>
+                    </a>
                 </ul>
-            </div>
+            </section>
         </div>
     )
 }
