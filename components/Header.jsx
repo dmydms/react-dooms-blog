712d9f4f300aa5de12382a01fede75c839faa784
dhdh3311 <dhdh3311@naver.com>
2021-09-26 00:15:23 +0900

    refactor: 리팩토링

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,17 +3,18 @@ import styles from './Header.module.scss'
 import Profile from './Profile'
 
 export default function Header() {
-
     return (
         <>
+            <Head>
+                <title>DOOM's DEV WORLD</title>
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css"/>
+                <script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"/>
+            </Head>
             <div className={styles.header_container}>
-                <Head>
-                    <title>DOOM's DEV WORLD</title>
-                    <link rel="icon" href="/favicon.ico" />
-                </Head>
-                <heaer className={styles.header}>
+                <header className={styles.header}>
                     <h1>{`DOOM's DEV WORLD`}</h1>
-                </heaer>
+                </header>
                 <Profile />
             </div>
         </>
