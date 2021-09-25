1fd0562106aa623ded45322b4f0097775cb7bb7d
dhdh3311 <dhdh3311@naver.com>
2021-09-25 23:47:54 +0900

    feat: TIL Component ssr 해제

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,17 @@
 import Header from '../components/Header'
+import dynamic from 'next/dynamic'
+
+const DynamicComponentWithNoSSR = dynamic(
+  () => import('../components/TIL'),
+  { ssr: false }
+)
+
 
 export default function Home() {
-  
   return (
     <>
       <Header />
+      <DynamicComponentWithNoSSR />
     </>
   )
 }
