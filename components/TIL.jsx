1e8a0e38d2b0227e43bcee7a4033fb205132965e
dhdh3311 <dhdh3311@naver.com>
2021-09-25 23:47:16 +0900

    feat: TIL 컴포넌트 추가

---
+++ components/TIL.jsx
@@ -0,0 +1,52 @@
+import styles from './TIL.module.scss'
+import { Queue } from '../utils/dataStruct'
+import { useState } from 'react'
+
+export default function TIL() {
+    const [curCellIndex, setCurCellIndex] = useState(0)
+    const CellContents = [{
+        'title': '2021-09-01 TIL',
+        'summary': 'blahblah',
+        'path': '/til/2021-09-01',
+    },{
+        'title': '2021-09-02 TIL',
+        'summary': 'blahblah2',
+        'path': '/til/2021-09-02',
+    },{
+        'title': '2021-09-03 TIL',
+        'summary': 'blahblah3',
+        'path': '/til/2021-09-03',
+    },{
+        'title': '2021-09-04 TIL',
+        'summary': 'blahblah4',
+        'path': '/til/2021-09-04',
+    },{
+        'title': '2021-09-04 TIL',
+        'summary': 'blahblah4',
+        'path': '/til/2021-09-04',
+    },]
+
+    const carouselCells = CellContents.map((content, i) => (
+        <div key={i}
+            className={styles.carousel_cell}>
+            {content.title}<br/>
+            {content.summary}<br/>
+            {content.path}
+        </div>
+    ))
+
+    console.log(carouselCells)
+    return (
+        <div className={styles.root}>
+            <h2>TIL</h2>
+            <div className={styles.carousel_wrapper}>
+                <div className={styles.carousel}>
+                    {carouselCells}
+                </div>
+
+                <div className={styles.carousel_left_btn}></div>
+                <div className={styles.carousel_right_btn}></div>
+            </div>
+        </div>
+    )
+}
